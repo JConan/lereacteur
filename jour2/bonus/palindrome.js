9f746db38815e8895bfa01988ee354430c446be4
jconan <conanjahh@hotmail.com>
2021-03-29 17:42:44 +0200

    exercice sur les palindromes

--- jour2/bonus/palindrome.js
+++ jour2/bonus/palindrome.js
@@ -1,5 +1,23 @@
+function formatInputStr(str) {
+  return str
+    .toLowerCase()
+    .split("")
+    .filter((char) => [" "].indexOf(char) < 0)
+    .join("");
+}
+
+function reverseStr(str) {
+  let result = "";
+  for (let i = 1; i <= str.length; i++) {
+    result += str.charAt(str.length - i);
+  }
+  return result;
+}
+
 function palindrome(str) {
-  return String.prototype.replaceAll;
+  const formattedStr = formatInputStr(str);
+  return reverseStr(formattedStr) === formattedStr;
 }
 
 console.log(palindrome("Aromate et Amora"));
+console.log(palindrome("Un bon snob nu"));
