6cf7707b9269d476ac866562155916ca5929d77b
jconan <conanjahh@hotmail.com>
2021-04-29 21:41:31 +0200

    add 2 tests for invalid inputs

--- 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.test.ts
+++ 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.test.ts
@@ -1,9 +1,8 @@
 import supertest from "supertest";
 import express from "express";
-import GuessNumber from "./app.route"
+import GuessNumber from "./app.route";
 
 describe("API - Guess random number from 0 to 100", () => {
-  
   it("should respond : less", async () => {
     await createGuessNumberApp()
       .get("/?num=12")
@@ -13,27 +12,45 @@ describe("API - Guess random number from 0 to 100", () => {
       });
   });
 
-  it("should respond: more", async() => {
+  it("should respond: more", async () => {
     await createGuessNumberApp(15)
       .get("/?num=12")
       .expect(200)
-      .then(response => {
-          expect(response.body).toEqual({ result: "C'est plus !" })
-      })
-})
-it("should respond: winner", async() => {
+      .then((response) => {
+        expect(response.body).toEqual({ result: "C'est plus !" });
+      });
+  });
+
+  it("should respond: winner", async () => {
     await createGuessNumberApp(12)
       .get("/?num=12")
       .expect(200)
-      .then(response => {
-          expect(response.body).toEqual({ result: "C'est gagné !" })
-      })
-})
+      .then((response) => {
+        expect(response.body).toEqual({ result: "C'est gagné !" });
+      });
+  });
 
+  it("should check for invalid input : not a number", async () => {
+    await createGuessNumberApp()
+      .get("/?num=a")
+      .expect(400)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "no number was sent" });
+      });
+  });
 
+  it("should check for invalid input : outbound number", async () => {
+    await createGuessNumberApp()
+      .get("/?num=101")
+      .expect(400)
+      .then((response) => {
+        expect(response.body).toEqual({
+          message: "the number must be between 0 and 100.",
+        });
+      });
+  });
 });
 
-
 function createGuessNumberApp(number: number = 0) {
-    return supertest(express().use(GuessNumber(number)));
+  return supertest(express().use(GuessNumber(number)));
 }
--- 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.ts
+++ 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.ts
@@ -1,33 +1,40 @@
-import {Router, Request, Response} from "express"
+import { Router, Request, Response } from "express";
 
-const router = Router()
+const router = Router();
 
 let _numberToGuess = 0;
 
-
-
-interface GuessNumberRequest extends Request{
-    query : {
-        num: string
-    }
+interface GuessNumberRequest extends Request {
+  query: {
+    num: string;
+  };
 }
 
+router.get("/", (request: { query: { num: string } }, response: Response) => {
+  const userInput: number = Number(request.query.num);
 
-router.get("/", (request: {query:{num: string}}, response: Response) => {
-    const userInput: number = Number(request.query.num)
-    
-    if(userInput > _numberToGuess ){
-        response.send({result: "C'est moins !"})
-    }if (userInput === _numberToGuess) {
+  if (isNaN(userInput)) {
+    response.status(400).send({ message: "no number was sent" });
+    return;
+  }
 
-        response.send({result: "C'est gagné !"})
-    }else{
-        response.send({result: "C'est plus !"})
+  if (userInput >= 0 && userInput <= 100) {
+    if (userInput > _numberToGuess) {
+      response.send({ result: "C'est moins !" });
     }
-
-})
+    if (userInput === _numberToGuess) {
+      response.send({ result: "C'est gagné !" });
+    } else {
+      response.send({ result: "C'est plus !" });
+    }
+  } else {
+    response
+      .status(400)
+      .send({ message: "the number must be between 0 and 100." });
+  }
+});
 
 export default (numberToGuess: number) => {
-    _numberToGuess = numberToGuess
-    return router
-}
+  _numberToGuess = numberToGuess;
+  return router;
+};
