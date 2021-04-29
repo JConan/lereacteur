27249ec69302bea83ffcdeca2c1c2db5cfb25ecb
jconan <conanjahh@hotmail.com>
2021-04-29 21:27:38 +0200

    add 2 tests and validate all 3 nominal cases

--- 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.test.ts
+++ 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.test.ts
@@ -1,18 +1,39 @@
 import supertest from "supertest";
 import express from "express";
-import appRoute from "./app.route"
+import GuessNumber from "./app.route"
 
 describe("API - Guess random number from 0 to 100", () => {
+  
   it("should respond : less", async () => {
-    const app = express();
-    app.use(appRoute)
-    const appTest = supertest(app);
-
-    await appTest
+    await createGuessNumberApp()
       .get("/?num=12")
       .expect(200)
       .then((response) => {
         expect(response.body).toEqual({ result: "C'est moins !" });
       });
   });
+
+  it("should respond: more", async() => {
+    await createGuessNumberApp(15)
+      .get("/?num=12")
+      .expect(200)
+      .then(response => {
+          expect(response.body).toEqual({ result: "C'est plus !" })
+      })
+})
+it("should respond: winner", async() => {
+    await createGuessNumberApp(12)
+      .get("/?num=12")
+      .expect(200)
+      .then(response => {
+          expect(response.body).toEqual({ result: "C'est gagné !" })
+      })
+})
+
+
 });
+
+
+function createGuessNumberApp(number: number = 0) {
+    return supertest(express().use(GuessNumber(number)));
+}
--- 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.ts
+++ 02-serveur-et-base-de-données/jour1/more-or-less/src/app.route.ts
@@ -2,8 +2,32 @@ import {Router, Request, Response} from "express"
 
 const router = Router()
 
-router.get("/", (request: Request, response: Response) => {
-    response.send({result: "C'est moins !"})
+let _numberToGuess = 0;
+
+
+
+interface GuessNumberRequest extends Request{
+    query : {
+        num: string
+    }
+}
+
+
+router.get("/", (request: {query:{num: string}}, response: Response) => {
+    const userInput: number = Number(request.query.num)
+    
+    if(userInput > _numberToGuess ){
+        response.send({result: "C'est moins !"})
+    }if (userInput === _numberToGuess) {
+
+        response.send({result: "C'est gagné !"})
+    }else{
+        response.send({result: "C'est plus !"})
+    }
+
 })
 
-export default router
+export default (numberToGuess: number) => {
+    _numberToGuess = numberToGuess
+    return router
+}
