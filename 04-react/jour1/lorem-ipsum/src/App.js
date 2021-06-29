fc3240e55a19a347bb945a7566b47835f124f0a5
jconan <conanjahh@hotmail.com>
2021-06-29 20:45:52 +0200

    create component content

--- 04-react/jour1/lorem-ipsum/src/App.js
+++ 04-react/jour1/lorem-ipsum/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './components/Content';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>
+    <>
+        <Content title="Lorem Ipsum" description="Magna amet ea duis duis quis excepteur ipsum. Pariatur consectetur enim amet cupidatat laboris magna duis laborum minim Lorem enim in. Ea proident consequat Lorem aliquip tempor exercitation minim tempor magna. Non esse fugiat Lorem do duis qui. Ad dolor labore laboris nisi anim consectetur veniam consectetur proident laboris culpa." />
+        <Content title="Lorem Ipsum" description="Magna amet ea duis duis quis excepteur ipsum. Pariatur consectetur enim amet cupidatat laboris magna duis laborum minim Lorem enim in. Ea proident consequat Lorem aliquip tempor exercitation minim tempor magna. Non esse fugiat Lorem do duis qui. Ad dolor labore laboris nisi anim consectetur veniam consectetur proident laboris culpa." />
+    </>
 
 export default App;
---
+++ 04-react/jour1/lorem-ipsum/src/components/Content.js
@@ -0,0 +1,6 @@
+const Content = (props) => <div className="Content">
+    <h1 className="Title">{props.title}</h1>
+    <div className="Description">{props.description}</div>
+</div>
+
+export default Content;
